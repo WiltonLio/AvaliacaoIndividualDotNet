9cb371795a740d8900cabda50bd3a92962c597bc
WiltonLio <wilton.lio@gmail.com>
2023-11-29 10:40:00 -0300

    Criando classes

---
+++ POGDev/Medico.cs
@@ -0,0 +1,14 @@
+namespace POGDev
+// Classes Médico
+//Nome
+//Data de Nascimento
+//CPF (tratar entrada com 11 dígitos)
+//CRM
+
+public class Medico
+{
+    public string Nome { get; set;}
+    public string DataNascimento { get; set;}
+    public string Cpf { get; set;}
+    public string Crm { get; set;}
+}
---
+++ POGDev/Paciente.cs
@@ -0,0 +1,18 @@
+namespace POGDev
+// Class Paciente:
+//Nome
+//Data de Nascimento
+//CPF (tratar entrada com 11 dígitos)
+//Sexo (masculino e feminino)
+//Sintomas
+
+Class Paciente
+{
+    public string Nome { get; set;}
+    public string DataNascimento { get; set;}
+    public string Cpf { get; set;}
+    public string Sexo { get; set;}
+    public string Sintomas { get; set;}
+
+    
+}
